7a170a9523850faed56016c50f596e7d138a0638
박정규 <srk99279@naver.com>
2022-07-25 22:38:39 +0900

    success

--- report.js
+++ report.js
@@ -1,62 +1,65 @@
 function solution(id_list, report, k) {
-    // 한번에 한명 신고
-    // 횟수 제한 x 서로 다른 유저 연속 신고 가능하나 최대 1회 처리
-    // k번 이상 정지 및 신고자에게 알림 메일
-    // 모든 신고 내용 취합 후 정지 메일 발송
-
-    // const k = 2
-    // const id_list = ["muzi", "frodo", "apeach", "neo"]
-    // const report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
-
     var answer = [];
-    const obj_id = {};
-    const objReport = {}
-    const banList = [];                     // ★추가★
 
-    // id_list를 객체로 변화
-    id_list.map(id => obj_id[id] = 0)
+    const idList = {};
+    const reportCount = {};
+    const banList = [];
 
-    // 중복된 신고 제거를 위한 중복 배열 제거
-    const set = new Set(report)
-    const newReport = [...set]
+    // 초기화
+    id_list.map(id => idList[id] = 0);
 
-
-    const arrReport = newReport.map(rep => rep.split(" "))
-
-
-    // 신고 당한 아이디 객체 생성 { 아이디: 횟수 }
-    arrReport.map(rep => {
-        if (!objReport[rep[1]]){
-        objReport[rep[1]] = 1           
-        } else if (objReport[rep[1]]) {
-        objReport[rep[1]] += 1            
+    const set = new Set(report);
+    const reportList = [...set];
+     
+    const updateReport = reportList.map(rep => rep.split(" "));
+    
+    updateReport.map(rep => {
+        if (!reportCount[rep[1]]) { // 0 False
+            reportCount[rep[1]] = 1;
+        } else if (reportCount[rep[1]]) {
+            reportCount[rep[1]]++;
         }
     })
 
-    console.log(objReport)
-    // 신고 당한 아이디 객체 중 k 조건에 부합하지 않는 객체 제거
-    Object.keys(objReport).map(key => {
-        if(objReport[key] >= k) {           // ★수정★
-            banList.push(key);              // ★수정★
+
+    // 밴 리스트에 k 횟수를 넘은 아이디만 추가
+    Object.keys(reportCount).map(key => {
+        if (reportCount[key] >= k) {    // value와 비교
+            banList.push(key);
+        }
+    })  
+
+    updateReport.map(rep => {
+        if (banList.includes(rep[1])) {
+            idList[rep[0]]++;
         }
     })
 
-    // 정지된 유저를 신고한 신고자 객체 생성 
-    // report에서 정지된 유저를 신고했던 유저 서칭 후 객체 값 + 1
-    // { 유저명: 정지시킨 신고 수}
-    arrReport.map(rep => {
-        if(banList.includes(rep[1])){       // ★수정★
-            obj_id[rep[0]] += 1
-        }
-    })
-
-    // obj_id 객체의 값을 return
-    answer = Object.values(obj_id)
+    answer = Object.values(idList);
+    console.log(answer);
     return answer;
 }
-
 
 solution(["muzi", "frodo", "apeach", "neo"],
 	["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],	2)
 
 solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3)
+
+
+/* shortest one
+function solution(id_list, report, k) {
+    let reports = [...new Set(report)].map(a=>{return a.split(' ')});
+    let counts = new Map();
+    for (const bad of reports){
+        counts.set(bad[1],counts.get(bad[1])+1||1)
+    }
+    let good = new Map();
+    for(const report of reports){
+        if(counts.get(report[1])>=k){
+            good.set(report[0],good.get(report[0])+1||1)
+        }
+    }
+    let answer = id_list.map(a=>good.get(a)||0)
+    return answer;
+}
+*/
