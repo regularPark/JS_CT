52ef86529c06f06c312062bb227140180f194e65
박정규 <srk99279@naver.com>
2022-07-29 14:10:02 +0900

    2020 kakao

--- keypad.js
+++ keypad.js
@@ -5,17 +5,47 @@ function solution(numbers, hand) {
     let rHand = 12;
 
     numbers.map(num => {
+        if (num === 0) {
+            num = 11;
+        }
         if (num % 3 === 1) {
             answer += "L";
-            console.log(parseInt(num / 3));
+            lHand = num;
         } else if (num % 3 === 0) {
             answer += "R";
-            console.log(parseInt(num / 3));
+            rHand = num;
+        } else {
+            const lHandDist = calDistance(handPos(num), handPos(lHand));
+            const rHandDist = calDistance(handPos(num), handPos(rHand));
+
+            if (lHandDist === rHandDist) {
+                if (hand === "left") {
+                    answer += "L";
+                    lHand = num;
+                } else {
+                    answer += "R";
+                    rHand = num;
+                }
+            } else if (lHandDist > rHandDist) {
+                answer += "R";
+                rHand = num;
+            } else {
+                answer += "L";
+                lHand = num;
+            }
         }
     })
-
-
+    console.log(rHand, lHand)
+    console.log(answer);
     return answer;
+}
+
+function handPos(num) {
+    return [Math.floor((num - 1) / 3), (num - 1) % 3];
+}
+
+function calDistance(a, b) {
+    return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
 }
 
 solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5])
