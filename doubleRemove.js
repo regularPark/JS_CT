fd33053ca341514a625209f50582bc4d7332d84d
박정규 <srk99279@naver.com>
2022-08-18 17:52:21 +0900

    짝지어 제거하기

---
+++ doubleRemove.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    let arr = [];
+    let pos = -1;
+    for (let i = 0; i < s.length; i++) {
+        arr.push(s[i]);
+        ++pos;
+        if (arr[pos] === arr[pos - 1]) {
+            arr.pop();
+            arr.pop();
+            pos -= 2;
+        }
+    }
+
+    return arr.length > 0 ? 0 : 1;
+}
+
+solution('baabaa')
