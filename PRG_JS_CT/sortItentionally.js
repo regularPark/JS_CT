af59009ddb5be9a206de898a812be57ad143c3d4
박정규 <srk99279@naver.com>
2022-09-17 20:14:31 +0900

    [프로그래머스]문자열 내 마음대로 정렬하기

---
+++ PRG_JS_CT/sortItentionally.js
@@ -0,0 +1,10 @@
+const solution = (s, n) => {
+  return s.sort((a, b) => {
+    if (a[n] < b[n]) return -1;
+    else if (a[n] > b[n]) return 1;
+    else return a < b ? -1 : 1;
+  });
+};
+
+// console.log(solution(["sun", "bed", "car"], 1));
+console.log(solution(["abce", "abcd", "cdx"], 2));
