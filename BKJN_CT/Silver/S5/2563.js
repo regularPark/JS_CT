eb3503249c476bf6671856c91d53c1dc83c2243e
박정규 <srk99279@naver.com>
2022-12-30 19:20:44 +0900

    feat : [백준 S5] 2563번 색종이
    1. 색종이가 차지하는 가장 큰 x, y좌표를 구하고, 그 크기에 해당하는 0으로 초기화된 2차원 배열을 생성한다.
    2. 색종이가 차지하고 있을 좌표에 0이 존재할 때 1로 재할당하며, 동시에 answer++ 연산을 해주면 넓이를 도출할 수 있다.

--- BKJN_CT/Silver/S5/2563.js
+++ BKJN_CT/Silver/S5/2563.js
@@ -5,6 +5,7 @@ const input = require("fs")
   .split("\n")
   .map((x) => x.split(" ").map(Number));
 
+let answer = 0;
 let n = input.shift();
 let xMax = -1;
 let yMax = -1;
@@ -13,14 +14,23 @@ for (let i = 0; i < n; i++) {
   yMax = Math.max(yMax, input[i][1] + 10);
 }
 
-let board = new Array(xMax).fill(new Array(yMax).fill(0));
+let board = Array.from(Array(xMax), () => Array(yMax).fill(0));
 
 for (let i = 0; i < n; i++) {
   for (let j = input[i][0]; j < input[i][0] + 10; j++) {
     for (let k = input[i][1]; k < input[i][1] + 10; k++) {
-      board[k][j] = 1;
+      if (board[j][k] == 0) {
+        board[j][k] = 1;
+        answer++;
+      }
     }
   }
 }
-console.log(input);
-console.log(board);
+
+// for (let i = 0; i < board.length; i++) {
+//   for (let j = 0; j < board[0].length; j++) {
+//     if (board[i][j] == 1) answer++;
+//   }
+// }
+
+console.log(answer);
