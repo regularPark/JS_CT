982be8d80b5d77c9057bce4e2468adef8fe3d3eb
박정규 <srk99279@naver.com>
2023-01-03 17:51:18 +0900

    feat : [백준 S3] 15650번 N과 M (2)
    DFS를 이용하여 풀이하였고, 15649번 문제와 다른 점은 수열이 오름차순이어야 한다는 점이다.
    그러기 위해서는 연산마다 증가하는 index를 매개변수로 이용하여야 한다.

---
+++ BKJN_CT/Silver/S3/15650.js
@@ -0,0 +1,30 @@
+const [N, M] = require("fs")
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const visited = new Array(N);
+let nums = [];
+let answer = "";
+
+function dfs(idx, cnt) {
+  if (cnt === M) {
+    answer += `${nums.join(" ")}\n`;
+    return;
+  }
+
+  for (let i = idx; i < N; i++) {
+    if (visited[i] === true) continue;
+    visited[i] = true;
+    nums.push(i + 1);
+    dfs(i, cnt + 1);
+    nums.pop();
+    visited[i] = false;
+  }
+}
+
+dfs(0, 0);
+
+console.log(answer.trim());
