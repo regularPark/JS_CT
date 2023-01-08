d67be7ec367172e4a27c45e8d2ada52bfb3073ed
박정규 <regular_park@bagjeong-gyuui-MacBookAir.local>
2023-01-08 23:15:37 +0900

    feat : [백준 S4] 1021번 회전하는 큐
    console.log로 확인

--- BKJN_CT/Silver/S4/1021.js
+++ BKJN_CT/Silver/S4/1021.js
@@ -5,3 +5,19 @@ const input = require("fs")
   .split("\n");
 
 console.log(input);
+
+const [N, M] = input
+  .shift()
+  .split(" ")
+  .map((x) => +x);
+
+console.log(N, M);
+
+const numbers = input
+  .shift()
+  .split(" ")
+  .map((x) => +x);
+
+console.log(numbers);
+
+// deque를 어떻게 구성할지..?
