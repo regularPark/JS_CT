a91d00c2b36accbbb8756745fa63036c8fa8ed64
regularPark <srk99279@naver.com>
2023-01-10 17:41:13 +0900

    fix : 계단 두 개 오르는 부분 값 수정
    초기 dp 0, 1, 2값 수정 및 계단 두 개 오르는 값 계산 부분 fix.

--- BKJN_CT/Silver/S3/2579.js
+++ BKJN_CT/Silver/S3/2579.js
@@ -9,11 +9,12 @@ let n = input.shift();
 
 let dp = Array(n).fill(0);
 dp[0] = input[0];
-dp[1] = input[0] + input[1];
-dp[2] = Math.max(input[0], input[0] + input[1]);
+dp[1] = Math.max(input[0] + input[1], input[1]);
+dp[2] = Math.max(input[0] + input[2], input[1] + input[2]);
 
 for (let i = 3; i < n; i += 1) {
-  dp[i] = Math.max(dp[i - 2] + input[i], dp[i - 3] + input[i]);
+  dp[i] = Math.max(dp[i - 2] + input[i], dp[i - 3] + input[i - 1] + input[i]);
 }
 
-console.log(dp);
+// console.log(dp);
+console.log(dp[n - 1]);
