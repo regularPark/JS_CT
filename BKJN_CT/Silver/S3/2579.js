f684b2d69279b728cd26cb4e49a57626d9ee8630
regularPark <srk99279@naver.com>
2023-01-10 17:30:19 +0900

    feat : [백준 S3] 2579번 계단 오르기
    dp를 이용한 풀이.
    1. 0으로 채운 dp 배열을 만든다.
    2. dp 배열의 0,1,2 인덱스를 문제의 기준에 따라 미리 계산 & 할당
    3. 두 칸 이상 연속할 수 없는 것에 유의하여 input의 원소들을 i와 i-1까지 더하여 본다.

---
+++ BKJN_CT/Silver/S3/2579.js
@@ -0,0 +1,19 @@
+const input = require("fs")
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((x) => +x);
+
+let n = input.shift();
+
+let dp = Array(n).fill(0);
+dp[0] = input[0];
+dp[1] = input[0] + input[1];
+dp[2] = Math.max(input[0], input[0] + input[1]);
+
+for (let i = 3; i < n; i += 1) {
+  dp[i] = Math.max(dp[i - 2] + input[i], dp[i - 3] + input[i]);
+}
+
+console.log(dp);
