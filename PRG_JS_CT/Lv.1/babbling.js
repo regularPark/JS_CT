59a04b209ce0f8af9a88f8d31f88fbc8cfdbd7c2
regularPark <srk99279@naver.com>
2023-01-20 19:41:20 +0900

    feat : [프로그래머스 Lv.1] 옹알이(2)
    정규식을 통한 풀이.

--- PRG_JS_CT/Lv.1/babbling.js
+++ PRG_JS_CT/Lv.1/babbling.js
@@ -17,3 +17,14 @@ const solution = (babbling) => {
 };
 
 console.log(solution(["aya", "yee", "u", "maa"]));
+
+/** using regex */
+function solution(babbling) {
+  const regexp1 = /(aya|ye|woo|ma)\1+/;
+  const regexp2 = /^(aya|ye|woo|ma)+$/;
+
+  return babbling.reduce(
+    (ans, word) => (!regexp1.test(word) && regexp2.test(word) ? ++ans : ans),
+    0
+  );
+}
