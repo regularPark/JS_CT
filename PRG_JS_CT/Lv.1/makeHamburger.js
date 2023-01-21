a833bc8ed3fb5c4b72f93156dfe617b35d67caae
regularPark <srk99279@naver.com>
2023-01-21 16:43:49 +0900

    feat : [프로그래머스 Lv.1] 햄버거 만들기
    문제 풀이 당시 초기에 구상했던 풀이 방식과 같은 코드를 참고하여 작성.

--- PRG_JS_CT/Lv.1/makeHamburger.js
+++ PRG_JS_CT/Lv.1/makeHamburger.js
@@ -19,3 +19,18 @@ const solution = (ingredient) => {
 };
 
 console.log(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]));
+
+/** 초기 구상했던 코드와 매우 유사한 코드를 가져와 보았다. */
+function solution(ingredient) {
+  let count = 0;
+
+  for (let i = 0; i < ingredient.length; i++) {
+    if (ingredient.slice(i, i + 4).join("") === "1231") {
+      count++;
+      ingredient.splice(i, 4);
+      i -= 3;
+    }
+  }
+
+  return count;
+}
