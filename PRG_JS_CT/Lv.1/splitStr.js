215717e1922c7f2f946043f277622d63b1c05928
regularPark <srk99279@naver.com>
2023-01-22 22:31:12 +0900

    feat : [프로그래머스 Lv.1] 문자열 나누기

--- PRG_JS_CT/Lv.1/splitStr.js
+++ PRG_JS_CT/Lv.1/splitStr.js
@@ -3,17 +3,18 @@ const solution = (s) => {
   let x = 0;
   let notX = 0;
   let tmp = "";
-  for (let spell of s) {
+  for (let i = 0; i < s.length; i++) {
     if (tmp == "") {
-      tmp = spell;
+      tmp = s[i];
       x += 1;
-    } else if (x != notX && tmp != spell) {
+    } else if (x != notX && tmp != s[i]) {
       notX += 1;
-    }
+    } else if (s[i] == tmp) x += 1;
+
     if (x == notX) {
       tmp = "";
       answer += 1;
-    }
+    } else if (i == s.length - 1) answer += 1;
   }
   return answer;
 };
