ee33e6c4e56a2de4369fda29c93f3649e6da3886
regularPark <srk99279@naver.com>
2023-01-30 17:28:35 +0900

    refactor : getting shorter
    it not includes definition but expressions are in Math.max() function.

--- PRG_JS_CT/Lv.2/sliceArray.js
+++ PRG_JS_CT/Lv.2/sliceArray.js
@@ -2,9 +2,7 @@ const solution = (n, left, right) => {
   let answer = [];
 
   for (let i = left; i <= right; i++) {
-    let share = parseInt(i / n);
-    let remainder = i % n;
-    answer.push(Math.max(share, remainder) + 1);
+    answer.push(Math.max(parseInt(i / n), i % n) + 1);
   }
 
   return answer;
