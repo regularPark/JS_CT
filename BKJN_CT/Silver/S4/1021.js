1f6e06ef6509f701da027836a69dc64fa36ca207
regularPark <srk99279@naver.com>
2023-01-31 15:56:55 +0900

    feat : [백준 S4] 회전하는 큐
    원소는 중복되지 않기 때문에 indexOf를 이용하여 간단하게 위치를 파악가능
    그 때문에 배열의 정중앙을 가리키는 인덱스와 찾고자하는 원소의 인덱스를 알고 있을 때, 더욱 쉽게 찾을 수 있음.
    그 점을 이용하여 answer를 도출해냄.

--- BKJN_CT/Silver/S4/1021.js
+++ BKJN_CT/Silver/S4/1021.js
@@ -9,13 +9,54 @@ const [N, M] = input
   .split(" ")
   .map((x) => +x);
 
-console.log(N, M);
-
 const numbers = input
   .shift()
   .split(" ")
   .map((x) => +x);
 
-console.log(numbers);
+let queue = [];
 
-// deque를 어떻게 구성할지..?
+for (let i = 0; i < N; i++) {
+  queue.push(i + 1);
+}
+
+const take = () => {
+  queue.shift();
+};
+
+const popLeft = () => {
+  let tmp = queue.shift();
+  queue.push(tmp);
+};
+
+const popRight = () => {
+  let tmp = queue.pop();
+  queue.unshift(tmp);
+};
+
+let answer = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  let target = numbers[i];
+  if (target === queue[0]) {
+    take();
+  } else {
+    let pos = queue.indexOf(target);
+    let mid = queue.length / 2;
+
+    if (pos <= mid) {
+      for (let j = 0; j < pos; j++) {
+        popLeft();
+        answer++;
+      }
+    } else {
+      for (let k = 0; k < queue.length - pos; k++) {
+        popRight();
+        answer++;
+      }
+    }
+    take();
+  }
+}
+
+console.log(answer);
