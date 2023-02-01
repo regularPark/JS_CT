0f29791ef0e6628b6d94ccfcbf1692c32af3035b
regularPark <srk99279@naver.com>
2023-02-01 21:33:34 +0900

    feat : [프로그래머스 Lv.2] 할인 행사
    문자열 검사 알고리즘을 이용하면 될 것으로 생각됨. 이후에 구현할 것.

---
+++ PRG_JS_CT/Lv.2/discount.js
@@ -0,0 +1,30 @@
+const solution = (want, number, discount) => {
+  let answer = 0;
+  let start = 0;
+  for (let i = 0; i < discount.length; i++) {}
+
+  return answer;
+};
+
+console.log(
+  solution(
+    ["banana", "apple", "rice", "pork", "pot"],
+    [3, 2, 2, 2, 1],
+    [
+      "chicken",
+      "apple",
+      "apple",
+      "banana",
+      "rice",
+      "apple",
+      "pork",
+      "banana",
+      "pork",
+      "rice",
+      "pot",
+      "banana",
+      "apple",
+      "banana",
+    ]
+  )
+);
